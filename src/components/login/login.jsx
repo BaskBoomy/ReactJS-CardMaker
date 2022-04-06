60c9bf5d0402449a850894b34c460de7324969f5
Jack <whdgurtpqms@naver.com>
2022-04-06 17:29:02 +0900

    Add Llogin component

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
-
+import styles from './login.module.css';
 const Login = ({authService}) =>{
     const onLogin = (event) =>{
         authService
         .login(event.currentTarget.textContent)
         .then(console.log)
     }
+    const onLogout = () => {
+
+    }
     return(
-        <section>
-            <Header/>
-            <section>
+        <section className={styles.login}>
+            <Header onLogout={onLogout}/>
+            <section >
                 <h1>Login</h1>
-                <ul>
-                    <li><button onClick={onLogin}>Google</button></li>
-                    <li><button onClick={onLogin}>Github</button></li>
+                <ul className={styles.list}>
+                    <li className={styles.item}><button className={styles.button} onClick={onLogin}>Google</button></li>
+                    <li className={styles.item}><button className={styles.button} onClick={onLogin}>Github</button></li>
                 </ul>
             </section>
             <Footer/>
