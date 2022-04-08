af50fb76a89f422c864b6f421f6d8547fa954cb3
Jack <whdgurtpqms@naver.com>
2022-04-08 16:12:55 +0900

    handle state change on editor

---
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '../button/button';
+import ImageFileInput from '../image_file_input/image_file_input';
+import styles from './card_edit_form.module.css';
+const CardEditForm = ({card, updateCard, deleteCard}) => {
+    const {name, company, title, email, message, theme, fileName, fileURL} = card;
+    const onChange = (event) => {
+        if(event.currentTarget == null){
+            return;
+        }
+        event.preventDefault();
+        updateCard({
+            ...card,
+            [event.currentTarget.name]: event.currentTarget.value,
+        })
+    }
+    const onSubmit = () => {
+        deleteCard(card);
+    };
+    return(
+        <form className={styles.form}>
+            <input className={styles.input} type="text" name="name" value={name} onChange={onChange}/>
+            <input className={styles.input} type="text" name="company" value={company} onChange={onChange} />
+            <select className={styles.select} name="theme" value={theme} onChange={onChange}>
+                <option value="Light">light</option>
+                <option value="Dark">dark</option>
+                <option value="Colorful">colorful</option>
+            </select>
+            <input className={styles.input} type="text" name="title" value={title} onChange={onChange} />
+            <input className={styles.input} type="text" name="email" value={email} onChange={onChange} />
+            <textarea className={styles.textarea} name="message" value={message} onChange={onChange}/>
+            <div className={styles.fileInput}>
+                <ImageFileInput/>
+            </div>
+                <Button name="Delete" onClick={onSubmit}/>
+        </form>
+    );
+}
+
+export default CardEditForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Card from '../card/card';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
 import Header from '../header/header';
@@ -7,8 +8,8 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({authService}) => {
-    const [cards,setCards] = useState([
-        {
+    const [cards,setCards] = useState({
+        '1':{
             id: '1',
             name: 'Jack1',
             company: 'Samsung',
@@ -19,7 +20,7 @@ const Maker = ({authService}) => {
             fileName: 'jack',
             fileURL: 'jack.png'
         },
-        {
+        '2':{
             id: '2',
             name: 'Jack2',
             company: 'Samsung',
@@ -30,7 +31,7 @@ const Maker = ({authService}) => {
             fileName: 'jack',
             fileURL: null
         },
-        {
+        '3':{
             id: '3',
             name: 'Jack3',
             company: 'Samsung',
@@ -41,7 +42,7 @@ const Maker = ({authService}) => {
             fileName: 'jack',
             fileURL: null
         }
-    ]);
+    });
     const navigate = useNavigate();
     const onLogout = () => {
         authService.logout();
@@ -54,11 +55,36 @@ const Maker = ({authService}) => {
             }
         })
     })
+
+    const createOrUpdateCard = (card) => {
+        /*
+        방법 1
+        //updated에 cards의 데이터를 모두 복사
+        const updated = {...cards};
+        //받아온 card의 데이터를 key를 활용하여 정보를 수정한다.
+        updated[card.id] = card;
+        //수정한 정보를 set해준다.
+        setCards(updated);
+        */
+       //방법 2
+        setCards(cards => {
+            const updated = {...cards};
+            updated[card.id] = card;
+            return updated;
+        });
+    }
+    const deleteCard = (card) => {
+        setCards(cards => {
+            const updated = {...cards};
+            delete updated[card.id];
+            return updated;
+        });
+    }
     return(
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor cards={cards}/>
+                <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard}/>
                 <Preview cards={cards}/>
             </div>
             <Footer/>
