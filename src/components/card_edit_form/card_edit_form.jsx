fdafc71e41cc8f491890f10f1ef2be49f71d5a6a
Jack <whdgurtpqms@naver.com>
2022-04-09 12:01:35 +0900

    add loading spinner for file input

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '../button/button';
 import styles from './card_edit_form.module.css';
 const CardEditForm = ({ FileInput, card, updateCard, deleteCard}) => {
-    const {name, company, title, email, message, theme, fileName, fileURL} = card;
+    const {name, company, title, email, message, theme, fileName} = card;
     const onChange = (event) => {
         if(event.currentTarget == null){
             return;
